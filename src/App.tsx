f433f0dfce8c017b7fbb4391eed8cde54c9145c0
Anna Groshkova <anna.matvyeyenko@gmail.com>
2026-01-02 11:22:32 +0100

    routing skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import type { ReactNode } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { StrictMode } from 'react'
 import './App.css'
+import Home from './views/Home'
 
-export default function App(): ReactNode {
+export default function   App(): ReactNode {
   return (
-    <div>
-      hello
-    </div>
+      <StrictMode>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </StrictMode>
+
   )
 }
 
