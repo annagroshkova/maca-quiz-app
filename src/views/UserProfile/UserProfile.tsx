8a10ede7f601aa2c4c2cbdf86c4029aefc82315d
Anna Groshkova <anna.matvyeyenko@gmail.com>
2026-01-27 22:04:34 +0100

    UserProfile styling half way

--- src/views/UserProfile/UserProfile.tsx
+++ src/views/UserProfile/UserProfile.tsx
@@ -2,6 +2,7 @@ import { useUser, avatarColors } from "../../context/UserContext";
 import SubmitButton from "../../components/SubmitButton/SubmitButton";
 import { useState, useEffect } from "react";
 import Avatar from "../../components/Avatar/Avatar";
+import MainWrapper from "../MainWrapper";
 import useQuizNavigation from "../../hooks/useQuizNavigation";
 import { motion } from "motion/react";
 import Header from "../../components/Header/Header";
@@ -28,7 +29,7 @@ export default function UserProfile() {
   };
 
   return (
-    <section className="startpage">
+    <>
       <Header
         backButton={true}
         backButtonProps={{
@@ -37,79 +38,72 @@ export default function UserProfile() {
           },
           children: (
             <img
-              src="go-back-icon-192-solid.svg"
-              alt="Go back icon"
+              src='go-back-icon-192-solid.svg'
+              alt='Go back icon'
               style={{ height: "100%" }}
             />
           ),
         }}
       />
-      <div>
-        <motion.div
-          className="userContainer "
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          transition={{ duration: 0.3 }}
-        >
+      ;
+      <MainWrapper>
+        <div className='userInner'>
           {user.name ? (
-            <Avatar name={user.name} size={175} bgColor={user.bgColor} />
+            <Avatar name={user.name} size={120} bgColor={user.bgColor} />
           ) : null}
-          <div className="userInner">
-            <h1 className="userName">{user.name}</h1>
-            <div className="userColors">
-              {avatarColors.map((color) => (
-                <button
-                  key={color}
-                  style={{
-                    backgroundColor: color,
-                    width: "40px",
-                    height: "40px",
-                    borderRadius: "50%",
-                    cursor: "pointer",
-                    border: "none",
-                  }}
-                  aria-label={`Select color ${color}`}
-                  onClick={() => handleColorClick(color)}
-                />
-              ))}
-            </div>
-            <div className="userWrapper">
-              <div className="userScore">
-                <h3 className="scoreDisplay">Last Score: {user.lastScore}</h3>
-                <h3 className="scoreDisplay">Best Score: {user.bestScore}</h3>
-              </div>
-              <form className="nameChangeForm" onSubmit={handleNameSubmit}>
-                <label
-                  htmlFor="name"
-                  style={{ color: "#382B76", fontWeight: "bold" }}
-                >
-                  Change name?
-                </label>
-                <input
-                  id="name"
-                  type="text"
-                  value={displayName}
-                  onChange={(e) => setDisplayName(e.target.value)}
-                  placeholder="Enter new name"
-                  className="userNameInput"
-                />
-                <button
-                  className="userNameButton"
-                  type="submit"
-                  disabled={!displayName.trim()}
-                >
-                  Save
-                </button>
-              </form>
-            </div>
-
-            <SubmitButton variant="default" onClick={goToRules}>
-              Rules
-            </SubmitButton>
+          <h1 className='userName'>{user.name}</h1>
+          <div className='userColors'>
+            {avatarColors.map((color) => (
+              <button
+                key={color}
+                style={{
+                  backgroundColor: color,
+                  width: "40px",
+                  height: "40px",
+                  borderRadius: "50%",
+                  cursor: "pointer",
+                  border: "none",
+                }}
+                aria-label={`Select color ${color}`}
+                onClick={() => handleColorClick(color)}
+              />
+            ))}
           </div>
-        </motion.div>
-      </div>
-    </section>
+          <div className='userWrapper'>
+            <div className='userScore'>
+              <h3 className='scoreDisplay'>Last Score: {user.lastScore}</h3>
+              <h3 className='scoreDisplay'>Best Score: {user.bestScore}</h3>
+            </div>
+            <form className='nameChangeForm' onSubmit={handleNameSubmit}>
+              <label
+                htmlFor='name'
+                style={{ color: "#382B76", fontWeight: "bold" }}
+              >
+                Change name?
+              </label>
+              <input
+                id='name'
+                type='text'
+                value={displayName}
+                onChange={(e) => setDisplayName(e.target.value)}
+                placeholder='Enter new name'
+                className='userNameInput'
+              />
+              <button
+                className='userNameButton'
+                type='submit'
+                disabled={!displayName.trim()}
+              >
+                Save
+              </button>
+            </form>
+          </div>
+
+          <SubmitButton variant='default' onClick={goToRules}>
+            Rules
+          </SubmitButton>
+        </div>
+      </MainWrapper>
+    </>
   );
 }
